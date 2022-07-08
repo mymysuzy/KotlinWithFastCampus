a7e982220a24f87295f0bd20000e5eebf4a3e116
mymysuzy <mymysuzy0627@gmail.com>
2022-07-08 17:47:54 +0900

    TIL upload - learned about weight, Circular Positioning and Chain feature in ConstraintLayout

--- Kotlin/app/src/main/java/org/techtown/kotlin/MainActivity.kt
+++ Kotlin/app/src/main/java/org/techtown/kotlin/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.homework1)
+        setContentView(R.layout.constraintlayout_4)
     }
 }
